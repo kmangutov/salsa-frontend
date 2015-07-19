0399723eb3ac7c7b1653c5271ae05271297f2c9c
kmangutov <kmangutov@gmail.com>
2015-07-18 19:41:24 -0700

    updates ehehe

---
+++ app/scripts/directives/person.js
@@ -0,0 +1,18 @@
+
+
+
+angular.module('topazApp')
+  .directive('person', function () {
+    return {
+      restrict: 'E',
+      templateUrl: "views/directive-person.html",
+      scope: {
+        id: '@'
+      },
+      controller: function($scope) {
+        $scope.name = "Kirill Mangutov";
+        $scope.pictureUrl = "https://scontent-lax1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/1607113_491012347714488_7875067251027324117_n.jpg?oh=28d7fd64ad76d919a75ee7a2994892f8&oe=564D2002";
+
+      }
+    };
+  });    
