ff59617d187af965f6828d2f1f89e9b01e47ce4d
kmangutov <kmangutov@gmail.com>
2015-07-18 19:57:17 -0700

    work on display

--- app/scripts/directives/person.js
+++ app/scripts/directives/person.js
@@ -10,7 +10,7 @@ angular.module('topazApp')
         id: '@'
       },
       controller: function($scope) {
-        $scope.name = "Kirill Mangutov";
+        $scope.name = "Kirill";
         $scope.pictureUrl = "https://scontent-lax1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/1607113_491012347714488_7875067251027324117_n.jpg?oh=28d7fd64ad76d919a75ee7a2994892f8&oe=564D2002";
 
       }
