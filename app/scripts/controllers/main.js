a7b86a8bb335204076b45e49da42003bf7ef7d39
kmangutov <kmangutov@gmail.com>
2015-07-18 17:15:23 -0700

    pick title font

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,9 +7,37 @@
  * # MainCtrl
  * Controller of the topazApp
  */
+
+
 angular.module('topazApp')
-  .controller('MainCtrl', function ($scope, Facebook) {
-    
+  .controller('MainCtrl', function ($scope, Facebook) {    
+
+    var loadFb = function() {
+
+      var structure = {};
+
+      console.log("loadFb()");
+      Facebook.api('/me', function(response) {
+        structure['id'] = response.id;
+        structure['name'] = response.name;
+
+        Facebook.api('/me/posts', function(response) {
+          var data = response.data;
+
+          console.log(data);
+          structure['posts'] = [];
+          data.forEach(function(post) {
+            console.log("###" + post.message);
+            if(post.message) {
+              structure['posts'].push(post.message);
+            }
+          });
+
+          console.log("data structure: " + JSON.stringify(structure));
+        });
+      });
+    }
+
     $scope.IntentLogin = function() {
       Facebook.login(function(response) {
         $scope.status = JSON.stringify(response);
@@ -17,10 +45,12 @@ angular.module('topazApp')
 
         if(response.status == "connected") {
           $scope.accessToken = response.authResponse.accessToken;
-          Facebook.api('/me/posts', function(response) {
+          
+          loadFb(Facebook);
+          /*Facebook.api('/me/posts', function(response) {
             $scope.status = JSON.stringify(response);
             console.log(response);
-          });
+          });*/
 
 
         } 
